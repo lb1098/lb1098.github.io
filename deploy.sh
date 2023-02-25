658f94d22e022f3fbf9cc2a5c06c5db4fd1c9a80
lb1098 <gao571470893@qq.com>
2023-02-25 12:52:06 +0800

    push

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ git add -A
 git commit -m 'deploy'
 
 git push -f git@github.com:lb1098/lb1098.github.io.git master
+git push -f git@gitee.com:lb1098/lb1098.gitee.io.git master
 
 cd ../
 rm -rf public
